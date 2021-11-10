31b846e39cb26802de252a4c7a06d17bf1d61ee3
OfekShaked <ofekshaked1999@gmail.com>
2021-11-10 11:47:51 +0200

    all

---
+++ LibraryRenewed.Design/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,60 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryRenewed.Design.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public DisplayBooksOptionsPageViewModel DisplayBooksOptionsPage
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<DisplayBooksOptionsPageViewModel>();
+            }
+        }
+        public MainPageViewModel MainPage
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainPageViewModel>();
+            }
+        }
+        public ManagerMainPageViewModel ManagerMainPage
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<ManagerMainPageViewModel>();
+            }
+        }
+        public RegisterPageViewModel RegisterPage
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<RegisterPageViewModel>();
+            }
+        }
+        public ReportPageViewModel ReportPage
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<ReportPageViewModel>();
+            }
+        }
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<DisplayBooksOptionsPageViewModel>();
+            SimpleIoc.Default.Register<MainPageViewModel>();
+            SimpleIoc.Default.Register<ManagerMainPageViewModel>();
+            SimpleIoc.Default.Register<RegisterPageViewModel>();
+            SimpleIoc.Default.Register<ReportPageViewModel>();
+
+        }
+    }
+}
